32d046b17185e18108e4f9a68df2b678db7cfc86
Luiz Felipe <lflimeira02@gmail.com>
2019-10-21 03:27:26 -0200

    test(get-user): create test for get user

--- tests/unit/controllers/user.test.js
+++ tests/unit/controllers/user.test.js
@@ -5,12 +5,14 @@ const { UserError } = require('../../../src/helpers/errors')
 jest.mock('../../../src/database', () => ({
   Users: {
     findAll: jest.fn(),
+    findOne: jest.fn(),
   },
 }))
 
 describe('User', () => {
   beforeEach(() => {
     database.Users.findAll.mockReset()
+    database.Users.findOne.mockReset()
   })
 
   describe('List function', () => {
@@ -44,6 +46,51 @@ describe('User', () => {
 
       expect(error).toEqual(new UserError('Error listing user'))
       expect(database.Users.findAll).toBeCalledTimes(1)
+    })
+  })
+  describe('Get function', () => {
+    const usersMock = {
+      id: 1,
+      first_name: 'Ash',
+      last_name: 'Ketchium',
+      date_of_birth: '1995-04-01',
+    }
+    it('should return a user', async () => {
+      database.Users.findOne.mockReturnValueOnce(Promise.resolve({
+        id: 1,
+        first_name: 'Ash',
+        last_name: 'Ketchium',
+        date_of_birth: new Date('1995-04-01'),
+      }))
+
+      const params = {
+        request: {
+          id: usersMock.id,
+        },
+      }
+
+      const users = await userController.get(params, jest.fn())
+
+      expect(users).toEqual(usersMock)
+      expect(database.Users.findOne).toBeCalledTimes(1)
+    })
+
+    it('should return an error', async () => {
+      database.Users.findOne.mockImplementation(() => {
+        throw new Error('Database error')
+      })
+
+
+      const params = {
+        request: {
+          id: usersMock.id,
+        },
+      }
+
+      const error = await userController.get(params, jest.fn())
+
+      expect(error).toEqual(new UserError('Error getting user'))
+      expect(database.Users.findOne).toBeCalledTimes(1)
     })
   })
 })
